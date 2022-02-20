0563e5ebab26363aaab7c731fec5e23381e4647f
roeeavir <44678382+roeeavir@users.noreply.github.com>
2022-02-20 21:16:33 +0200

    Created basic map layout

--- src/components/Land.js
+++ src/components/Land.js
@@ -1,8 +1,33 @@
-function Land(props){
+function WriteLand(props){
     return (
-        <div className="land">
-            <button className="btn btn-primary">{props.text}</button>
+        <div class="land">
+            <button id="landBtn" style={{background: props.color}}>{props.text}</button>
         </div>
     );}
 
-export default Land;
+class Land {
+    constructor(id, type, x, y, owner, price) {
+        this.id = id;
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.price = price;
+        this.owner = owner;
+        this.color = this.setColorByType()
+    }
+
+    setColorByType() {
+        if (this.type === "nLand") {
+            return "brown";
+        } else if (this.type === "road") {
+            return "grey";
+        } else if (this.type === "park") {
+            return "green";
+        }
+
+    }
+}
+
+export { Land };
+
+export default WriteLand;
